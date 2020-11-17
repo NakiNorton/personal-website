21b999c687920421bf1350277d40b741871e42dc
Stephanie Norton <steph.jane.norton@gmail.com>
2020-11-17 10:29:10 -0700

    Create about me component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import './AboutMe.css'
+
+const AboutMe = () => {
+  return (
+  
+      <section className='about-me'>
+      <h2 className='page-heading'>About me..</h2>
+      <Paper>All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer. All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer. 
+       <br />
+        <br />
+      All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer. All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer.</Paper> 
+      </section>
+
+  )
+
+}
+
+export default AboutMe
