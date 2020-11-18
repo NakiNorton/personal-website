3a85c66712d48454f17e34f0349976292d4d2036
Stephanie Norton <steph.jane.norton@gmail.com>
2020-11-18 13:32:55 -0700

    Edit content and add images and styling

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import Paper from '@material-ui/core/Paper'
+// import Paper from '@material-ui/core/Paper'
+import { data } from '../../data'
 import './AboutMe.css'
+
 
 const AboutMe = () => {
   return (
-  
-      <section className='about-me'>
-      <h2 className='page-heading'>About me..</h2>
-      <Paper>All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer. All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer. 
-       <br />
-        <br />
-      All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer. All about me...   hello my name is Stephanie, I am from New Zealand and I've lived in Denver for the last yer.</Paper> 
-      </section>
-
+    <section className='about-me'>
+    <h2 className='page-heading'>About me..</h2>
+      <p>{data.aboutMeSection0}</p>
+      <p>{data.aboutMeSection1}</p>
+      <p>{data.aboutMeSection2}</p>
+      <p>{data.aboutMeSection3}</p>
+      <img className='photo' src={data.photos[0]} alt='With my foster dog' />
+      <img className='photo' src={data.photos[1]} alt='Hiking in Colorado' />
+    </section>
   )
-
 }
 
 export default AboutMe
