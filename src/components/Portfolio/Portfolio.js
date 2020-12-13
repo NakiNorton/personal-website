bfacee65d7933c2b99d51ee232c3acc6fc774afc
Stephanie Norton <steph.jane.norton@gmail.com>
2020-12-13 12:51:07 -0700

    Add expandable features button

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { data } from '../../data'
 import DotsMobileStepper from '../stepper'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './Portfolio.css'
 
 const Portfolio = () => {
   const [currentIndex, getNextProject] = useState(0);
+  const [featuresAreVisible, displayFeatures] = useState(false);
 
   const handleNext = () => {
     getNextProject((prevActiveStep) => prevActiveStep + 1);
@@ -13,6 +15,10 @@ const Portfolio = () => {
   const handleBack = () => {
     getNextProject((prevActiveStep) => prevActiveStep - 1);
   };
+
+  const showFeatures = () => {
+    !featuresAreVisible ? displayFeatures(true) : displayFeatures(false)
+  }
 
   return (
     <div className='portfolio'>
@@ -24,7 +30,13 @@ const Portfolio = () => {
       <section className='portfolio-container'>
         <div className='projects'>
           <h3>{data.projects[currentIndex].name}</h3>
-          <article><span style={{ fontWeight: '600' }}>Overview:</span> {data.projects[currentIndex].description}</article>
+          <article><span style={{ fontWeight: '600' }}>Overview:</span> {data.projects[currentIndex].description}
+          <br />
+            <button className='features-btn' onClick={() => showFeatures()}>Features <span><ExpandMoreIcon fontSize='medium'></ExpandMoreIcon></span></button>
+          </article>
+          { featuresAreVisible &&
+            <article><span style={{ fontWeight: '600' }}></span> {data.projects[currentIndex].features}</article>
+          }
           <br />
           <article><span style={{ fontWeight: '600' }}>Tech stack: </span> 
            {data.projects[currentIndex].techStack}</article>
--- src/data.js
+++ src/data.js
@@ -25,7 +25,9 @@ export const data = {
       name: 'LienFlash',
       image: lienFlashImage,
       className: 'img lienflash-img',
-      description: 'LienFlash is a client backed app built for a team of lawyers who have experienced frustration in dealing with the Lien filing process. LienFlash is a Progressive Web App, it can be used in the browser or it can be installed on a desktop or a mobile device. The app allows a user to login as either a general user (client) or an admin.  Users can create a new job, track that job in the lien process, and submit the job info to an administrator when they are ready to submit a Notice of Intent, Lien, or Release of Lien. Users are sent SMS notifications to remind them of upcoming deadlines. We are currently building out the Administrators dashboard using the Material UI Data Grid component. The admin will be able to see all jobs (across all users) displayed on the Data Grid, click on a job to see all of the job details, and update the status of a job once they\'ve processed the paperwork. The updated job status will then be reflected on the users/clients account. This app was developed with two front-end developers (myself and one other) and a back-end team.',
+      description: 'LienFlash is a client backed app built for a team of lawyers who have experienced frustration in dealing with the Lien filing process. This app was developed with two front-end developers (myself and one other) and a back-end team.', 
+      features: 
+       'LienFlash is a Progressive Web App, it can be used in the browser or it can be installed on a desktop or a mobile device. The app allows a user to login as either a general user (client) or an admin.  Users can create a new job, track that job in the lien process, and submit the job info to an administrator when they are ready to submit a Notice of Intent, Lien, or Release of Lien. Users are sent SMS notifications to remind them of upcoming deadlines. We are currently building out the Administrators dashboard using the Material UI Data Grid component. The admin will be able to see all jobs (across all users) displayed on the Data Grid, click on a job to see all of the job details, and update the status of a job once they\'ve processed the paperwork. The updated job status will then be reflected on the users/clients account.',
       techStack: 'React w.Hooks, React Router, JavaScript, JSON Web Token(JWT), Fetch API, JSX, PWA, Redux, SASS/SCSS, Material UI, Continuous Integration(Travis CI), Heroku, Unit and Integration testing with: Jest, React Testing Library, Redux mock store'
     },
     {
@@ -33,7 +35,8 @@ export const data = {
       name: 'Edible',
       image: edible,
       className: 'img', 
-      description: "Edible. is a web application designed for plant lovers who want to start foraging for wild plants. The user is able to filter the plants being displayed by 'edible part' categories; roots, seeds, flowers, leaves, fruits, and also search for plants by common name or scientific name. The user can save plants to their Saved Plants page, and also remove plants from this page. This app was a solo project developed with an open API, Trefle.",
+      description: "Edible. is a web application designed for plant lovers who want to start foraging for wild plants. This app was a solo project developed with an open API, Trefle.", 
+      features: "The user is able to filter the plants being displayed by 'edible part' categories; roots, seeds, flowers, leaves, fruits, and also search for plants by common name or scientific name. The user can save plants to their Saved Plants page, and also remove plants from this page.",
       techStack: 'React, React Router,  JavaScript, JSX, Fetch API, Redux, SASS/SCSS, Unit and Integration testing with: Jest, React Testing Library, Redux mock store'
     },
     {
@@ -41,7 +44,8 @@ export const data = {
       name: 'ReadMe',
       image: readme,
       className: 'img', 
-      description: "ReadMe is a book tracking app for avid readers using the New York Times API. Users can view top selling books by genre, search for books by author or title, save books to their reading list, click on a book to view the books blurb, and click a button to be redirected to Amazon where they can purchase the book. This app was built in a team with three other front-end developers.",
+      description: "ReadMe is a book tracking app for avid readers using the New York Times API. This app was built in a team with three other front-end developers.",  
+      features: "Users can view top selling books by genre, search for books by author or title, save books to their reading list, click on a book to view the books blurb, and click a button to be redirected to Amazon where they can purchase the book.",
       techStack: 'React, React Router,  JavaScript, JSX, Fetch API, Redux, CSS, Heroku, Unit and Integration testing with: Jest, React Testing Library, Redux mock store'
     },
     {
@@ -49,7 +53,8 @@ export const data = {
       name: 'Rancid Tomatillos',
       image: rancidTomatillos,
       className: 'img',
-      description: "Rancid Tomatillos is a movie rating app where a user can login and rate movies, comment on movies, and favorite/unfavorite movies. If a user doesn't have a login, they can view movie details but they aren't able to share their own ratings or comments, and they do not have favoriting capabilities. This app was built for a pair project.",
+      description: "Rancid Tomatillos is a movie rating app built for a pair project.", 
+      features: "A user can login and rate movies, comment on movies, and favorite/unfavorite movies. If a user doesn't have a login, they can view movie details but they aren't able to share their own ratings or comments, and they do not have favoriting capabilities.",
       techStack: 'React, React Router,  JavaScript, JSX, Fetch API, CSS, Unit and Integration testing with: Jest, React Testing Library'
     },
     {
@@ -57,7 +62,8 @@ export const data = {
       name: 'Overlook Hotel',
       image: overlookhotel,
       className: 'img',
-      description: "Overlook is a hotel management tool for hotel customers and staff to manage room bookings. When logging in as a guest, the guest's name, bookings, and their total amount spent at the hotel are displayed. A guest can view all available rooms that meet their search criteria for date and room type, and make a new reservation. When logging in as a manager, the total amount of rooms available, total revenue for today's date, and percentage of occupied rooms for today will be displayed. A manager can search for a guest by name and view all of the guests bookings. This app was built as a solo project.",
+      description: "Overlook is a hotel management tool for hotel customers and staff to manage room bookings. This app was built as a solo project.",  
+      features: "When logging in as a guest, the guest's name, bookings, and their total amount spent at the hotel are displayed. A guest can view all available rooms that meet their search criteria for date and room type, and make a new reservation. When logging in as a manager, the total amount of rooms available, total revenue for today's date, and percentage of occupied rooms for today will be displayed. A manager can search for a guest by name and view all of the guests bookings.",
       techStack: 'JavaScript, HTML, CSS/SCSS, Fetch API, Test Driven Development with Mocha/Chai, Webpack'
     },
   ]
