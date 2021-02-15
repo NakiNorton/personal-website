72a007a3bb9224e859f551403b6f8d7c96ec65c1
Stephanie Norton <steph.jane.norton@gmail.com>
2021-02-15 09:19:15 -0700

    Remove rouge quotation marks

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -39,7 +39,7 @@ const AboutMe = () => {
   const displayStory = () => {
     return (
       <>
-        <p className="paragraph">I’m a graduate of Turing School of Software Design’s Front-End Software Engineering program which is a seven month immersive program focused on project-based learning. The 70-80 hrs a week I put into this program has enabled me to develop a solid foundation to kick-off my new career as a software engineer."</p>
+        <p className="paragraph">I’m a graduate of Turing School of Software Design’s Front-End Software Engineering program which is a seven month immersive program focused on project-based learning. The 70-80 hrs a week I put into this program has enabled me to develop a solid foundation to kick-off my new career as a software engineer.</p>
         <p className="paragraph">To be honest, this program was the hardest thing I’ve ever done in my life. But that’s why I got into tech; for the challenge, the fulfilment, and sense of purpose I feel when I’m challenged to grow both professionally and personally. With the continual innovation in tech there is no shortage of things to learn, and ways that I can use my skills to make a positive impact. I was also attracted to a career in software engineering as I’ve always loved being able to exercise my creativity with hands-on work (tinkering with woodwork, sewing, painting). When I code, I start to lose track of time and become fully engaged in what I'm doing - it's a great feeling.</p>
 
         <p className="paragraph">Prior to Turing... I grew up in New Zealand with dual New Zealand/USA citizenship and I bought a one-way ticket to Boston in 2015. I wanted to explore the USA and make the most of my dual citizenship. I ended up working in Boston for 4 years as an Executive Assistant in the consulting industry. After moving to Denver, CO (where I currently reside) in late 2019, I decided to address the nagging feeling that a career as an Executive Assistant wasn’t the right path to continue on. This led me to a period of self-reflection, career exploration, and online coding courses. I quickly fell in love with coding and after speaking to people in the industry I decided to commit to a career change. In early 2020 and enrolled at Turing for its project based learning, and longer ‘bootcamp’ program.</p>
@@ -52,9 +52,6 @@ const AboutMe = () => {
       <section className='section-about'>
         <article className="about__textbox">
           <h2 className='about__heading heading-secondary'>About Me 
-            {/* <span>
-              <img className='about__kiwi-img' src={kiwi} alt='kiwi bird' />
-            </span> */}
           </h2>
               <p className="paragraph" style={{ fontStyle: 'italic'}}>{data.aboutMeSection0}
               </p>
