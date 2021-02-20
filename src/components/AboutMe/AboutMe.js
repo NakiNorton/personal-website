58e345827533988e843fecded9280955d7c891d0
Stephanie Norton <steph.jane.norton@gmail.com>
2021-02-20 15:45:44 -0700

    Change to column layout

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -13,8 +13,8 @@ const AboutMe = () => {
 
   const useStyles = makeStyles((theme) => ({
     root: {
-      width: '300px',
-      height: '300px',
+      width: '250px',
+      height: '250px',
       opacity: '0.75',
       border: "5px solid white",
       margin: '0 auto',
@@ -44,32 +44,32 @@ const AboutMe = () => {
 
   return (
     <div className='about'>
-      <section className='section-about'>
-        <article className="about__textbox">
-          <h2 className='about__heading heading-secondary'>About Me 
-          </h2>
-              <p className="paragraph" style={{ fontStyle: 'italic'}}>{data.aboutMeSection0}
-              </p>
-              <p className="paragraph">I'm an empathetic software developer with a wealth of experience working both collaboratively and independently to deliver quality results. I love problem solving, variety in my work, and user-centric design.</p>
-    
-          <h3 className="heading-tertiary u-margin-bottom-small">Key Skills</h3>
-              <ul className='about__skill-list paragraph'>
-                <li>ReactJS, React Router, Hooks</li>
-                <li>Javascript ES5/ES6</li>
-                <li>API</li>
-                <li>Git / GitHub</li>
-                <li>Redux</li>
-                <li>HTML/CSS</li>
-                <li>SASS/SCSS</li>
-                <li>Test Assisted Development</li>
-              </ul>
-          <button className="expand-btn" onClick={() => showStory()}>My Story<span><ExpandMoreIcon fontSize='medium'></ExpandMoreIcon></span></button>
-          { story && displayStory() }
-        </article>
+      <h2 className='about__heading heading-secondary'>About</h2>
+      <div className='section-about'>
         <div className="avatar-container">
           <Avatar className={classes.root} src={linkedIn} alt="LinkedIn profile photo" />
         </div>
-      </section>
+
+        <div className="about__textbox">
+              <p className="paragraph">I'm an empathetic software developer with a wealth of experience working both collaboratively and independently to deliver quality results. I love problem solving, variety in my work, and user-centric design.</p>
+          <h3 className="heading-tertiary u-margin-bottom-small">Key Skills</h3>
+              <ul className='about__skill-list paragraph'>
+                <li className='pill'>React</li>
+                <li className='pill'>React Hooks</li>
+                <li className='pill'>Javascript ES5/ES6</li>
+                <li className='pill'>API</li>
+                <li className='pill'>Git/GitHub Workflow</li>
+                <li className='pill'>Redux</li>
+                <li className='pill'>HTML5</li>
+                <li className='pill'>Agile</li>
+                <li className='pill'>UI/UX</li>
+                <li className='pill'>Responsive Web Design</li>
+                <li className='pill'>CSS3</li>
+                <li className='pill'>SASS/SCSS</li>
+                <li className='pill'>Test Assisted Development</li>
+              </ul>
+        </div>
+      </div>
     </div>
   )
 }
