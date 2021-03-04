621ed58927be854314f1a2362238c877b61fa2f9
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-03 17:44:17 -0700

    Edit copy

--- src/data.js
+++ src/data.js
@@ -21,7 +21,7 @@ export const data = {
       description: 'LienFlash is a client backed app built for a team of lawyers who have experienced frustration in dealing with the Lien filing process. This app was developed with two front-end developers (myself and one other) and a back-end team.', 
       features: 
        'LienFlash is a Progressive Web App, it can be used in the browser or it can be installed on a desktop or a mobile device. The app allows a user to login as either a general user (client) or an admin.  Users can create a new job, track that job in the lien process, and submit the job info to an administrator when they are ready to submit a Notice of Intent, Lien, or Release of Lien. Users are sent SMS notifications to remind them of upcoming deadlines.',
-      techStack: ['React (w/ Hooks)', 'React Router', 'JSON Web Token (JWT)', 'Fetch API', 'PWA', 'Redux', 'SASS/SCSS', 'Material UI', 'Continuous Integration(Travis CI)', 'Heroku', 'Jest', 'React Testing Library', 'Redux mock store']
+      techStack: ['React (w/ Hooks), React Router', 'JSON Web Token (JWT)', 'Fetch API', 'PWA', 'Redux', 'SASS/SCSS', 'Material UI', 'Continuous Integration(Travis CI)', 'Jest', 'React Testing Library']
     },
     {
       id: 2,
@@ -31,7 +31,7 @@ export const data = {
       className: 'img', 
       description: "Edible. is a web application designed for plant lovers who want to start foraging for wild plants. This app was a solo project developed with an open API, Trefle.", 
       features: "The user is able to filter the plants being displayed by 'edible part' categories; roots, seeds, flowers, leaves, fruits, and also search for plants by common name or scientific name. The user can save plants to their Saved Plants page, and also remove plants from this page.",
-      techStack: ['React', 'React Router', 'Fetch API', 'Redux', 'SASS/SCSS', 'Jest', 'React Testing Library', 'Redux mock store']
+      techStack: ['React, React Router', 'Fetch API', 'Redux', 'SASS/SCSS', 'Jest', 'React Testing Library']
     },
     {
       id: 3,
@@ -41,7 +41,7 @@ export const data = {
       className: 'img', 
       description: "ReadMe is a book tracking app for avid readers using the New York Times API. This app was built in a team with three other front-end developers.",  
       features: "Users can view top selling books by genre, search for books by author or title, save books to their reading list, click on a book to view the books blurb, and click a button to be redirected to Amazon where they can purchase the book.",
-      techStack: ['React', 'React Router', 'Fetch API', 'Redux', 'CSS', 'Heroku', 'Jest', 'React Testing Library', 'Redux mock store']
+      techStack: ['React, React Router', 'Fetch API', 'Redux', 'CSS', 'Jest', 'React Testing Library']
     },
     // {
     //   id: 5,
