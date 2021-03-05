aba74b36f726458fe85c01d6082c795c16091bc1
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-05 07:20:27 -0700

    Precache images and add fadeIn animation to background img

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ButtonAppBar from '../Navigation/ButtonAppBar';
 import Header from '../Header/Header'
 import Portfolio from '../Portfolio/Portfolio'
 import AboutMe from '../AboutMe/AboutMe'
 import Contact from '../Contact/Contact'
 import '../../sass/main.scss';
+import '../../assets/domenico-gentile.jpg'
 
-function App() {
+const App = () => {
+  const [ isLoading, setIsLoading ] = useState(true)
+
+  const cacheImages = async (assets) => {
+    const promises = await assets.map(asset => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image()
+
+        img.asset = asset
+        img.onload = resolve()
+        img.onerror = reject()
+      })
+    })
+    await Promise.all(promises)
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    const images = ['../../assets/domenico-gentile.jpg', '../../assets/lienFlash-home.png'] 
+
+    cacheImages(images)
+  }, [isLoading])
+
   return (
     <main className="app">
       <ButtonAppBar />
-      <Header id="header"/>
+      <Header id="header" isLoading={isLoading}/>
       <AboutMe id="about" />
       <Portfolio id="portfolio" />
       <Contact id="contact" />
