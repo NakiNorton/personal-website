90aabce9cb613e4053525b9ff6c898066a009ae9
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-05 13:47:49 -0700

    Decrease image size to fix render issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,36 +5,13 @@ import Portfolio from '../Portfolio/Portfolio'
 import AboutMe from '../AboutMe/AboutMe'
 import Contact from '../Contact/Contact'
 import '../../sass/main.scss';
-import '../../assets/domenico-gentile.jpg'
+
 
 const App = () => {
-  const [ isLoading, setIsLoading ] = useState(true)
-
-  const cacheImages = async (assets) => {
-    const promises = await assets.map(asset => {
-      return new Promise(function (resolve, reject) {
-        const img = new Image()
-
-        img.asset = asset
-        img.onload = resolve()
-        img.onerror = reject()
-      })
-    })
-    await Promise.all(promises)
-
-    setIsLoading(false)
-  }
-
-  useEffect(() => {
-    const images = ['../../assets/domenico-gentile.jpg', '../../assets/lienFlash-home.png'] 
-
-    cacheImages(images)
-  }, [isLoading])
-
   return (
     <main className="app">
       <NavBar />
-      <LandingPage id="header" isLoading={isLoading}/>
+      <LandingPage id="header" />
       <AboutMe id="about" />
       <Portfolio id="portfolio" />
       <Contact id="contact" />
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,28 +3,21 @@ import BurgerMenu from './../Navigation/BurgerMenu'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
-const LandingPage = ({ isLoading }) => {
+const LandingPage = () => {
   return (
     <div className="header u-margin-top-medium">
       <div id="mobile">
         <BurgerMenu />
       </div>
       <div className="header__text-box">
-        { isLoading &&
-          <h1 style={{ color: 'lightgrey' }}>Loading . . .</h1>
-        }
-        { !isLoading &&
-        <>
-          <h1 className="heading-primary">
-            <span className="heading-primary--main">Stephanie Norton</span>
-            <span className="heading-primary--sub"> Frontend Software Engineer.</span>
-          </h1>
-          <div className='header__contact-icons'>
-            <a className='icon' href='https://www.linkedin.com/in/stephanie-norton-12888453/'><LinkedInIcon fontSize='inherit' /></a>
-            <a href='https://github.com/NakiNorton' className='icon'><GitHubIcon fontSize='inherit'/></a>
-          </div>
-          </>
-        }
+        <h1 className="heading-primary">
+          <span className="heading-primary--main">Stephanie Norton</span>
+          <span className="heading-primary--sub"> Frontend Software Engineer.</span>
+        </h1>
+        <div className='header__contact-icons'>
+          <a className='icon' href='https://www.linkedin.com/in/stephanie-norton-12888453/'><LinkedInIcon fontSize='inherit' /></a>
+          <a href='https://github.com/NakiNorton' className='icon'><GitHubIcon fontSize='inherit'/></a>
+        </div>
       </div>
     </div>
   )
