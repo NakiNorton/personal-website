edb535a3caa39fc61d77893338103d2b3ef6256c
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-11 10:15:25 -0700

    Move project heading above image

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,11 +8,12 @@ const Portfolio = () => {
     return data.projects.map(project => {
       return (
         <section className='card'>
+          <h3 className='project-title'>{project.name}</h3>
           <div className='img-container'>
             <img className={project.className} alt='gif of project' src={project.image} />
           </div>
           <section className='project-description'>
-            <h3 className='project-title'>{project.name}</h3>
+            
             <a className='ext-link' href={project.deployedUrl} target="_blank" rel="noreferrer">Deployed App</a> / <a className='ext-link' href={project.githubUrl} target="_blank" rel="noreferrer">View repo</a>
             <p className='paragraph'>{project.description}</p>
             <p className='paragraph' style={{ color: 'black', marginBottom: '0'}}>Tech Stack:</p>
